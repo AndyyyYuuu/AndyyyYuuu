a23bafe621c419fda2b11fe01209ebe5e8e5c071
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-05-21 14:59:12 -0700

    Add more even nicer stuff

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-window.addEventListener('scroll', function() {
-    const header = document.getElementById('title-page');
-    const placeholder = document.getElementById('placeholder');
-    const title = document.getElementById('main-title')
-    /*
-    if (window.scrollY > 80) {
-        title.style.transform = "scale("+Math.max(0.2, (1-window.scrollY))+")";
-    }*/
-    const start = 2000;
-    const end = 200;
-    const isStuck = header.offsetTop !== header.getBoundingClientRect().top;
-    
-    title.style.transform = "scale("+Math.min(1, Math.max(0.5, (start-window.scrollY)/end))+")";
+function titleIsStuck(){
+    const title = document.getElementById('main-title');
+    return parseInt(window.getComputedStyle(title).top) >= title.getBoundingClientRect().top;
+}
 
-    /*
-    if (window.scrollY > 80) {
-        header.classList.add('fixed-title');
-        placeholder.style.display = 'block';
+window.addEventListener('scroll', function() {
+    const title = document.getElementById('main-title');
+    console.log(titleIsStuck());
+    if (titleIsStuck()) {
+        title.classList.add('fixed-title');
     } else {
-        header.classList.remove('fixed-title');
-        placeholder.style.display = 'none';
-    }*/
+        title.classList.remove('fixed-title');
+    }
 });
+
+document.getElementById('main-title-text').addEventListener("click", toTop);
+
+function toTop(){
+    if (titleIsStuck()){
+        window.scrollTo(0, 0);
+    }
+    
+}
